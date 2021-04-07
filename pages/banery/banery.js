d5c6c3b78b957c56b7bc46cc765711874435b3a7
wangning <ningsy1017@163.com>
2021-04-08 00:25:47 +0800

    弹框判断

--- pages/banery/banery.js
+++ pages/banery/banery.js
@@ -7,35 +7,33 @@ let {
 } = wx.getSystemInfoSync();
 let ratio = screenWidth/750
 let back = wx.getBackgroundAudioManager();
+let audio = wx.createInnerAudioContext()
 let startX = 0     // 获取手指初始坐标
 let startY = 0 
 let x = 0
 let y = 0
-let old_left = 0   // 获得盒子原来的位置
-let old_top = 0
-let new_left = 0   // 获得盒子原来的位置
-let new_top = 0
 let cSys = {} // 当前系统配置
 let oldReact = [] // 拖拽元素宽高
 let defaultH = 100 // 缺省高度
 let defaultW = 100 // 缺省宽度
-
 import Dialog from '../../miniprogram_npm/vant-weapp/dialog/dialog';
-
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    audioSrc:'../pages/images/audio.mp3',
+    showBack:false, // 返回按钮
+    showReset:false, // 重玩按钮
     showImg:false,
-    defaultUrl:'http://i2.tiimg.com/735650/12183143c4a7f29b.png',
+    status:null,
+    backgroundUrl:'http://i2.tiimg.com/735650/12183143c4a7f29b.png',
     online:true,
-    showReset:false,
     old_left:310,
-    old_top:300,
-    new_left:10,
-    new_top:74,
+    old_top:290,
+    new_left:70,
+    new_top:740,
     x_old:'',
     y_old:'',
     x_new:'',
@@ -46,57 +44,27 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(windowHeight,windowWidth,pixelRatio,screenWidth)
-    
-    // this.backmusic()
-  //   var innerAudioContext = wx.createInnerAudioContext()
-  //   innerAudioContext.src='../pages/images/audio.mp3';
-  //   innerAudioContext.onPlay(() => {
-  //     console.log('开始播放')
-  // })
-
-  // innerAudioContext.onError((res) => {
-  //     console.error(res)
-
-  // })
-
-  // innerAudioContext.play();
-    this.setData({
-      status:1,
-      x_old:windowWidth-210,
-      y_old:windowHeight-420,
-      x_new:windowWidth-340,
-      y_new:windowHeight-160
-    });
-    const query = wx.createSelectorQuery().in(this)
-    query.select('.duster-img-old').boundingClientRect(res => {
-      cSys = wx.getSystemInfoSync()
-      oldReact = [res.width, res.height]
-      const top = Math.floor(cSys.windowHeight - oldReact[1] - defaultH)
-      const left = Math.floor(cSys.windowWidth - oldReact[0] - defaultW)
-      console.log(top,left)
-      this.setData({
-        top,
-        left
-      })
-    }).exec();
-    
+    console.log(windowHeight,windowWidth,pixelRatio,screenWidth);
   },
- 
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    this.dialog = this.selectComponent("#dialog");
-    
+    // this.dialog = this.selectComponent("#dialog");
+    audio.src = '../../pages/images/audio.mp3';
+    audio.obeyMuteSwitch = false
+    audio.play();
+    this.backmusic()
+
   },
   showDialog(){
     this.dialog.showDialog();
   },
   closeImg(){
     this.setData({
-      showImg:false
+      showImg:false,
+      showBack:this.data.status == 1?true:false,
+      showReset:this.data.status == 0?true:false
     })
   },
   //取消事件 
@@ -113,7 +81,7 @@ Page({
     player();
     function player() {
       back.title = "罗密欧与朱丽叶 ";   // 必须要有一个title
-      back.src = "../pages/images/audio.mp3";
+      back.src = ".././pages/images/audio.mp3";
       back.onEnded(() => {
         player();  // 音乐循环播放
       })
@@ -122,14 +90,28 @@ Page({
       })
     }
  },
+ /**
+  * 重新玩
+  */
   resetStart(){
     this.setData({
-      status:1,
       showReset:false,
-      x_old:windowWidth-210,
-      y_old:windowHeight-420,
-      x_new:windowWidth-340,
-      y_new:windowHeight-160
+      showBack:false,
+      status:null,
+      backgroundUrl:'http://i2.tiimg.com/735650/12183143c4a7f29b.png',
+      old_left:310,
+      old_top:290,
+      new_left:70,
+      new_top:740,
+
+    })
+  },
+  /**
+   * 返回相机界面
+   */
+  reback(){
+    wx.redirectTo({
+      url: '/pages/start/start',
     })
   },
   /**
@@ -147,8 +129,6 @@ Page({
       startY = e.changedTouches[0].pageY;
       x = e.currentTarget.offsetLeft;
       y = e.currentTarget.offsetTop;
-      console.log(x/pixelRatio)
-      console.log( e.changedTouches[0])
     },
     /**
      * 旧电池拖动
@@ -166,10 +146,21 @@ Page({
       if(top<0 || left<0){
         return false;
       }
+      let rpxLeft = left/ratio;
+      let rpxTop = top/ratio
       this.setData({
-        old_left:left/ratio,
-        old_top:top/ratio
+        old_left:rpxLeft,
+        old_top:rpxTop
       })
+     
+      if((rpxLeft>220 && rpxLeft<260)&&(rpxTop>260 && rpxTop<300)){
+        console.log('成功了')
+        this.setData({
+          backgroundUrl:'http://i2.tiimg.com/735650/dbedbf4cc4830b10.png',
+          status:1,
+          showImg:true
+        })
+      }
     },
     /**
      * 新电池拖动
@@ -182,10 +173,20 @@ Page({
       if(top<0 || left<0){
         return false;
       }
+      let rpxLeft = left/ratio;
+      let rpxTop = top/ratio
       this.setData({
-        new_left:left/ratio,
-        new_top:top/ratio
+        new_left:rpxLeft,
+        new_top:rpxTop
       })
+      console.log(rpxLeft,rpxTop)
+      if((rpxLeft>100&&rpxLeft<150)&&(rpxTop>800&&rpxTop<850)){
+        this.setData({
+          backgroundUrl:'http://i2.tiimg.com/735650/0a0c7f3b9e6b1c50.png',
+          status:0,
+          showImg:true
+        })
+      }
     },
      /**
      * 判断拖拽区域函数
